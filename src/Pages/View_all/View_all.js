d5df103083c70b524c27ccf75074c917c4ed3a87
Billy <billyjcatchpole@gmail.com>
2020-07-07 14:34:08 +0100

    Added a masonry grid package in order to display the notes properly in a grid and maintain proper spacing when they get expanded

--- src/Pages/View_all/View_all.js
+++ src/Pages/View_all/View_all.js
@@ -17,6 +17,9 @@ import { useSelector, useDispatch } from "react-redux"
 //functions
 import filter_notes_by_search from "./Functions/filter_notes_by_search"
 
+//external
+import Masonry from 'react-masonry-css'
+
 export const View_all = () => {
 
     //?selectors
@@ -29,21 +32,45 @@ export const View_all = () => {
     const [notes, set_notes] = useState([])//hold the notes to be displayed, all fetched initially, manipulated by searching and the toggle links
 
     //!Effects
-    useEffect(() => {dispatch(submit_form({ user_id: "5eecd941331a770017a74e44" }, "get_notes"))}, [])//fetch the notes on the first render of the page
-    useEffect(() => {if (response && response.data.notes) { set_notes(response.data.notes) }}, [response])//update the notes if the response changes
+    useEffect(() => { dispatch(submit_form({ user_id: "5eecd941331a770017a74e44" }, "get_notes")) }, [])//fetch the notes on the first render of the page
+    useEffect(() => { if (response && response.data.notes) { set_notes(response.data.notes) } }, [response])//update the notes if the response changes
+
+    // var items = [
+    //     { id: 1, name: 'My First Item' },
+    //     { id: 2, name: 'Another item' },
+    //     { id: 3, name: 'Third Item' },
+    //     { id: 4, name: 'Here is the Fourth' },
+    //     { id: 5, name: 'High Five' }
+    // ];
+
+    // // Convert array to JSX items
+    // items = items.map(function (item) {
+    //     return <div key={item.id}>{item.name}</div>
+    // });
 
 
     return (
 
         <div className={classes.container}>
 
-            <TopBar handle_search_input={(string)=> filter_notes_by_search(dispatch, string)} />
+            <TopBar handle_search_input={(string) => filter_notes_by_search(dispatch, string)} />
 
-            <div className={classes.note_container}>
+            {notes &&
+
+
+                <Masonry
+                    breakpointCols={3}
+                    className={classes.my_masonry_grid}
+                    columnClassName={classes.my_masonry_grid_column}>
+                    {notes.map((note, index) => <Note key={index} index={index} details={note} />)}
+                </Masonry>
+            }
+
+            {/* <div className={classes.note_container}>
 
                 {notes && notes.map((note, index) => <Note key={index} index={index} details={note} />)}
 
-            </div>
+            </div> */}
 
             <Nav />
 
