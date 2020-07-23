79f26fc1a865df3ac913ab26c18dfdcf451b37e2
Billy <billyjcatchpole@gmail.com>
2020-07-23 17:10:07 +0100

    Added full account management functionality

--- src/Pages/Account/components/Delete_account/Delete_account.js
+++ src/Pages/Account/components/Delete_account/Delete_account.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 //css
 import classes from './Delete_account.module.css'
@@ -6,7 +6,50 @@ import classes from './Delete_account.module.css'
 //components
 import Input from "../../../../Shared components/Input/Input"
 
+//redux hooks
+import { useDispatch, useSelector } from "react-redux"
+
+//redux action creators
+import { submit_form, clear_response } from '../../../../Store/Actions/0_submit_form_action'
+
+//external
+import {Redirect} from "react-router-dom"
+import alert from "easyalert"
+import { handle_logout } from '../../../../Store/Actions/2_authentication_action'
+
 export const Delete_account = () => {
+
+    //?selectors
+    const response = useSelector(state => state.form.response)
+    const user_id = useSelector(state => state.auth.user_id)
+
+    //-config
+    const dispatch = useDispatch()
+
+    //*states
+    const [password, set_password] = useState(null)
+    const [redirect, set_redirect] = useState(false)
+
+    const handle_click = () => {
+
+        dispatch(submit_form({ user_id: user_id, password: password }, "delete_user"))
+
+    }
+
+    useEffect(() => {
+
+        if (response && response.data.message === "Account deleted"){
+
+            set_redirect("/")
+
+            dispatch(handle_logout())
+
+            dispatch(clear_response())
+
+            alert("Your account has been deleted", "success")
+        } 
+
+    }, [response])
 
     return (
 
@@ -14,11 +57,16 @@ export const Delete_account = () => {
 
             <span className={classes.prompt_text}>Once deleted, your account cannot be recovered. Are you sure ?</span>
 
-            <Input type={"password"} visiblity_toggleable placeholder="Enter your password" label={"Password"}/>
+            <Input type={"password"} visiblity_toggleable placeholder="Enter your password" label={"Password"} value={password}
+                onChange={(e) => set_password(e.target.value)}
+            />
 
-            <div className={classes.button}>Delete my account</div>
+            {response && response.status > 400 && <div className={classes.error_message}>{response.data.message}</div>}
 
-            
+            <div className={classes.button} onClick={() => handle_click()}>Delete my account</div>
+
+            {redirect && <Redirect to={redirect} />}
+
         </div>
 
     )
