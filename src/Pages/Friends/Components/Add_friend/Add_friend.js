0d1498b3f03d45b7da6cc83112ea3b70cff7aa16
Billy <billyjcatchpole@gmail.com>
2020-07-25 10:11:06 +0100

    Added display of users when searching to add a friend

--- src/Pages/Friends/Components/Add_friend/Add_friend.js
+++ src/Pages/Friends/Components/Add_friend/Add_friend.js
@@ -1,18 +1,57 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 //css
 import classes from './Add_friend.module.css'
 
 //util
 import colours from '../../../../util/colours'
+import handle_column_assignment from "../../../../util/handle_column_assignment"
 
 //components
 import SearchBox from "../../../../Shared components/Top_bar/Components/Search_box"
+import User from "./Components/User"
+
+//redux hooks
+import { useSelector, useDispatch } from "react-redux"
+
+//redux action creators
+import { submit_form } from "../../../../Store/Actions/0_submit_form_action"
+
+//external
+import Masonry from 'react-masonry-css'
 
 export const Add_friend = () => {
 
-    const [search_value, set_search_value] = useState("")
+    //?selectors
+    const user_id = useSelector(state => state.auth.user_id)//grab the user id from redux
+    const response = useSelector(state => state.form.response)//grab the response from the api
 
+    //-config
+    const dispatch = useDispatch()//initialse the usedispatch hook
+
+    //*states
+    const [search_value, set_search_value] = useState("")//hold the value of the search box
+    const [users_to_display, set_users_to_display] = useState([])
+
+    //!effects
+    useEffect(() => {
+
+        if (!search_value) return set_users_to_display([])
+        dispatch(submit_form({ user_id: user_id, search_string: search_value }, "search_user"))
+
+    }, [search_value])
+
+    useEffect(() => {
+
+        if (response) set_users_to_display(response.data.users)
+
+    }, [response])
+
+    const handle_add_user = details => {
+
+        console.log(details)
+
+    }
     return (
 
         <div className={classes.container}>
@@ -28,6 +67,39 @@ export const Add_friend = () => {
 
             />
 
+            {users_to_display.length >= 1 &&
+
+
+                <Masonry
+
+                    breakpointCols={handle_column_assignment(users_to_display)}
+                    className={classes.my_masonry_grid}
+                    columnClassName={classes.my_masonry_grid_column}
+                    style={{ paddingBottom: "0px" }}>
+
+                    {
+
+                        users_to_display.map(user => <User details={user} key={user._id} onClick={(details)=> handle_add_user(details)}  />)
+
+                    }
+
+                </Masonry>
+
+
+
+
+
+
+
+
+
+                // <div className={classes.users_to_display_container}>
+
+                //     {users_to_display.map(user => <User details={user} key={user._id} />)}
+
+                // </div>
+
+            }
         </div>
 
     )
--- src/Shared components/Profile_image/Profile_image.js
+++ src/Shared components/Profile_image/Profile_image.js
@@ -12,7 +12,7 @@ import { fetch_image_url } from "../../Store/Actions/4_profile_image_handler"
 //assets
 import placeholder from "../../Assets/Icon/profile-placeholder.svg"
 
-export const Profile_image = () => {
+export const Profile_image = props => {
 
     //?selectors
     const user_id = useSelector(state => state.auth.user_id)//grab the user id from the reducer
@@ -28,17 +28,22 @@ export const Profile_image = () => {
     useEffect(() => {
 
         //if a userid is present, fetch the url for their profile picture
-        if (user_id) { dispatch(fetch_image_url(user_id)) }
+        if (user_id ) { dispatch(fetch_image_url(user_id)) }
 
         // eslint-disable-next-line
     }, [user_id, url])
 
     //if an image url is present, update the image url state to set the source of the image accordingly
-    useEffect(() => { if (url) set_image_url(url) }, [url])
+    useEffect(() => { 
+        
+        if(props.source) return set_image_url(props.source)
+        else if (url && !props.user) set_image_url(url) 
+    
+    }, [url])
 
     return (
 
-        <div className={classes.container}>
+        <div className={classes.container} style={{transform:`scale(${props.scale})`, marginLeft:props.marginLeft}}>
 
             <img src={image_url} alt="Your profile pic" className={classes.image} />
 
